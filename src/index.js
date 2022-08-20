b663b0445e91175c240c634a00ce80b7c7a53a1d
git\Aditya Nayak <nayak.aditya998@gmail.com>
2022-08-20 17:26:26 +0530

    Final Code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
-import { configurestore } from './store';
+import { configureStore } from './store';
 
-const store = configurestore();
-console.log('store', store.getState());
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import reducer from '../reducers/index';
+import reducer from '../reducers';
 
 let store;
 
-export function configurestore() {
+export function configureStore() {
   store = createStore(reducer, applyMiddleware(thunk, logger));
 
   return store;
